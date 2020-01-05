cf3c03d1338636eabb00c05c8999a0626ef3c511
David Öhman <dohman_92@hotmail.com>
2020-01-05 22:49:22 +0100

    Add navigation
    Setting up navigation for this project, safeArgs as well.

--- app/src/main/java/com/dohman/simplebudgetapp/ui/MainActivity.kt
+++ app/src/main/java/com/dohman/simplebudgetapp/ui/MainActivity.kt
@@ -2,12 +2,17 @@ package com.dohman.simplebudgetapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.dohman.simplebudgetapp.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.nav_host_fragment)
     }
 }
